a719713df8204bc6fde7cbb0a0606027b8a9da18
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-11-21 20:49:34 +0800

    0821

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,15 @@
 class Solution {
-    public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                for (int k = j+1; k < arr.length; k++) {
-                    if (Math.abs(arr[i] - arr[j]) <= a &&
-                            Math.abs(arr[j] - arr[k]) <= b &&
-                            Math.abs(arr[i] - arr[k]) <= c)
-                        result++;
-                }
+    public int[][] transpose(int[][] matrix) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        int[][] result = new int[column][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                result[j][i] = matrix[i][j];
             }
         }
+
         return result;
     }
 }
