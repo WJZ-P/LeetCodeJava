816c628c638df1963ffa426ffe23bbb9d5011ce6
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-11-22 09:17:00 +0800

    0821

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,22 @@
 class Solution {
-    public int[][] transpose(int[][] matrix) {
-        int row = matrix.length;
-        int column = matrix[0].length;
+    public int peakIndexInMountainArray(int[] arr) {
+        int half = arr.length / 2;
+        int left = 0, right = arr.length - 1;
 
-        int[][] result = new int[column][row];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                result[j][i] = matrix[i][j];
+        while (true) {
+            if (arr[half] > arr[half - 1] && arr[half] < arr[half + 1]) {
+                //说明需要往右上山。
+                left = half;
+                half = (half + right) / 2;
+
+            } else if (arr[half] < arr[half - 1] && arr[half] > arr[half + 1]) {
+                //说明需要往左上山。
+                right = half;
+                half = (half + left) / 2;
+            } else {
+                //说明已经到达山顶。
+                return half;
             }
         }
-
-        return result;
     }
 }
