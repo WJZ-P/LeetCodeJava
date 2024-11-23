4b04868d3dba0fbb6cd118468eef632d886dd830
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-11-23 18:28:40 +0800

    0821

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,16 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int half = arr.length / 2;
-        int left = 0, right = arr.length - 1;
-
-        while (true) {
-            if (arr[half] > arr[half - 1] && arr[half] < arr[half + 1]) {
-                //说明需要往右上山。
-                left = half;
-                half = (half + right) / 2;
-
-            } else if (arr[half] < arr[half - 1] && arr[half] > arr[half + 1]) {
-                //说明需要往左上山。
-                right = half;
-                half = (half + left) / 2;
-            } else {
-                //说明已经到达山顶。
-                return half;
-            }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]))//找到数字了
+                return new int[]{map.get(target - nums[i]), i};
+            else
+                map.put(nums[i], i);//记录下来
         }
+        return new int[0];
     }
 }
